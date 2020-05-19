17bbab502f05f2c169d36706776e2efc4b18db9d
Eliot McIntire <eliotmci@mail.ubc.ca>
2020-05-19 13:53:37 -0700

    use shim method -- should be OK with CRAN policies

--- R/setLibPaths.R
+++ R/setLibPaths.R
@@ -13,33 +13,51 @@
 #'
 #' @param libPaths A new path to append to, or replace all existing user
 #'   components of \code{.libPath()}
+#' @param algo Numeric, either 1 or 2, indicating which underlying algorithm
+#'   to use to set \code{.libPaths()}. \code{1} uses a defunct \code{.lib.loc}
+#'   and \code{2} uses a shim environment. Default is \code{2}. Both appear to
+#'   work on all systems tested.
 #' @inheritParams Require
+#' @return
+#' As when setting \code{options}, this will return the previous state of
+#' \code{.libPaths()} allowing the user to reset easily.
 #' @export
 #' @examples
 #' \dontrun{
-#' setLibPaths("~/newProjectLib") # will only have 2 paths
+#' orig <- setLibPaths("~/newProjectLib") # will only have 2 paths
+#' .libPaths() # see the 2 paths
+#' setLibPaths(orig) # reset
+#' .libPaths() # see the 2 original paths back
+#'
+#' # will have 2 or more paths
 #' setLibPaths("~/newProjectLib", standAlone = FALSE) # will have 2 or more paths
 #'
 #' }
-setLibPaths <- function(libPaths, standAlone = TRUE) {
+setLibPaths <- function(libPaths, standAlone = TRUE, algo = 2) {
   oldLibPaths <- .libPaths()
   libPaths <- checkPath(normPath(libPaths), create = TRUE)#, mustWork = TRUE)
 
-  #shim_fun <- .libPaths
-  #shim_env <- new.env(parent = environment(shim_fun))
-  if (isTRUE(standAlone)) {
-    assign(".lib.loc", unique(c(libPaths, tail(.libPaths(), 1))),
-           envir = environment(.libPaths))
-    #shim_env$.Library <- tail(.libPaths(), 1)
-  } else {
-    assign(".lib.loc", unique(c(libPaths, .libPaths())),
-           envir = environment(.libPaths))
-    #shim_env$.Library <- .libPaths()
+  if (algo == 1) {
+    if (isTRUE(standAlone)) {
+      assign(".lib.loc", unique(c(libPaths, tail(.libPaths(), 1))),
+             envir = environment(.libPaths))
+    } else {
+      assign(".lib.loc", unique(c(libPaths, .libPaths())),
+             envir = environment(.libPaths))
+    }
+  } else if (algo == 2) {
+
+    shim_fun <- .libPaths
+    shim_env <- new.env(parent = environment(shim_fun))
+    if (isTRUE(standAlone)) {
+      shim_env$.Library <- tail(.libPaths(), 1)
+    } else {
+      shim_env$.Library <- .libPaths()
+    }
+    shim_env$.Library.site <- character()
+
+    environment(shim_fun) <- shim_env
+    shim_fun(unique(libPaths))
   }
-  #shim_env$.Library.site <- character()
-
-  #environment(shim_fun) <- shim_env
-  #shim_fun(unique(libPaths))
-
   return(oldLibPaths)
 }
