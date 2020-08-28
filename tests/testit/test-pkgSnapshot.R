b9c74855b3789ce92973a42e56cc6511212f3271
Eliot McIntire <eliotmci@mail.ubc.ca>
2020-08-28 12:21:32 -0700

    Bump

--- tests/testit/test-pkgSnapshot.R
+++ tests/testit/test-pkgSnapshot.R
@@ -1,16 +1,20 @@
 if (interactive()) {
+  aa <- pkgSnapshot()
   # googledrive::drive_download(googledrive::as_id("1Yo_7nuIn580rKqBCeycssVBOoe_qb7oY"))
   # library(Require)
   if (file.exists("packageVersions.txt")) {
     fileNames <- list()
     baseFN <- "packageVersions"
-    fileNames[["fn0"]][["lp"]] <- file.path("~", baseFN)
+    tmpLibPath <- "~/tmpLibPath1"
+    fileNames[["fn0"]][["lp"]] <- file.path(tmpLibPath)
     fileNames[["fn0"]][["txt"]] <- paste0(baseFN, ".txt")
     try(setLibPaths(origLibPaths[[1]]))
-    options("Require.persistentPkgEnv" = TRUE, "Require.RPackageCache" = "~/._RPackageCache/", warn = 0,
-            install.packages.check.source = "never", "install.packages.compile.from.source" = "never")
+    options("Require.persistentPkgEnv" = TRUE, 
+            "Require.RPackageCache" = "~/._RPackageCache/", 
+            "install.packages.check.source" = "never", 
+            "install.packages.compile.from.source" = "never")
     origLibPaths <- list()
-    origLibPaths[[1]] <- setLibPaths(paste0(fileNames[["fn0"]][["lp"]], "_AA"))
+    origLibPaths[[1]] <- setLibPaths(paste0(fileNames[["fn0"]][["lp"]]))
     Require(packageVersionFile = fileNames[["fn0"]][["txt"]])   
     
     # Test
