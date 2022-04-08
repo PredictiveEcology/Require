516cdf506a925732defef51f728604d5320c1ca8
Eliot McIntire <eliotmcintire@gmail.com>
2022-04-07 17:23:25 -0700

    manual part of merge

--- R/Require-helpers.R
+++ R/Require-helpers.R
@@ -2,7 +2,7 @@ utils::globalVariables(c(
   "Archs", "AvailableVersion", "correctVersion", "destFile", "dup", "filepath",  "github", "hasVersionSpec",
   ".I", "i.neededFiles", "inequality", "installFromFac", "installOrder", "installResult", "isGitPkg",
   "keep", "keep2", "localType", "localFileName", "mtime", ".N", "N", "Names", "neededFiles", "newMtime",
-  "Package", "packageFullName", "repoLocation", "type", "version"
+  "Package", "packageFullName", "repoLocation", "tmpOrder", "type", "version"
 ))
 
   #' @details
