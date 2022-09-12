1b263df283470fc0f9736010543d5fd73bf5e361
Eliot McIntire <eliotmcintire@gmail.com>
2022-09-12 14:23:58 -0700

    rm some args in pkgDep that were not used

--- R/pkgDep.R
+++ R/pkgDep.R
@@ -34,9 +34,6 @@ utils::globalVariables(c(
 #' @param depends Logical. Include packages listed in "Depends". Default \code{TRUE}.
 #' @param imports Logical. Include packages listed in "Imports". Default \code{TRUE}.
 #' @param suggests Logical. Include packages listed in "Suggests". Default \code{FALSE}.
-#' @param enhances Logical. Include packages listed in "Enhances". Default \code{FALSE}.
-#' @param remotes Logical. Include packages listed in "Remotes". This is only relevant for GitHub packages.
-#'   Default \code{TRUE}.
 #' @param linkingTo Logical. Include packages listed in "LinkingTo". Default \code{TRUE}.
 #' @param recursive Logical. Should dependencies of dependencies be searched, recursively.
 #'                  NOTE: Dependencies of suggests will not be recursive. Default \code{TRUE}.
