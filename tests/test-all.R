1474466719a2f58eaf0f18a78db534256531f3a6
Eliot McIntire <eliotmcintire@gmail.com>
2022-09-26 11:34:58 -0700

    deal with "need to set CRAN option" on all cases

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,9 +1,17 @@
 library(testit)
 library(Require)
+optsListNew <- list()
+optsListPrev <- list()
+
 verbosity <- if (interactive()) 1 else -1
-opts <- options(Require.verbose = verbosity)
-unlink(dir(getOptionRPackageCache(), full.names = TRUE), recursive = TRUE)
-# optsCRAN <- options(repos = c(CRAN = "https://muug.ca/mirror/cran"))
-# optsCRAN <- options(repos = c(CRAN = "https://cloud.r-project.org"))
-test_pkg("Require")
-options(opts)
+optsListPrevLinux <- if (interactive()) setLinuxBinaryRepo() else NULL
+
+optsListNew <- modifyList2(optsListNew, list(Require.verbose = verbosity))
+if (is.null(getOption("repos")))
+  optsListNew <- modifyList2(optsListNew, list(repos = c(CRAN = "https://cloud.r-project.org")))
+optsListPrev <- options(optsListNew)
+optsListNew <- modifyList2(optsListNew, options("repos"))
+optsListPrev <- modifyList2(optsListPrev, optsListPrevLinux)
+#unlink(dir(getOptionRPackageCache(), full.names = TRUE), recursive = TRUE)
+#test_pkg("Require")
+# options(optsListPrev)
