abff881e96af57d80f7482adbad45ccd23ae23a6
Eliot McIntire <eliotmcintire@gmail.com>
2022-09-26 11:05:15 -0700

    verbosity -- keep low when not interactive

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,8 +1,7 @@
 library(testit)
 library(Require)
-opts <- options(Require.verbose = 2#,
-                #Require.RPackageCache = FALSE
-                )
+verbosity <- if (interactive()) 1 else -1
+opts <- options(Require.verbose = verbosity)
 unlink(dir(getOptionRPackageCache(), full.names = TRUE), recursive = TRUE)
 # optsCRAN <- options(repos = c(CRAN = "https://muug.ca/mirror/cran"))
 # optsCRAN <- options(repos = c(CRAN = "https://cloud.r-project.org"))
