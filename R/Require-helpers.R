339f6ac52f296f154d4b3ddedabde8c08549585a
Eliot McIntire <eliotmcintire@gmail.com>
2022-10-04 23:15:39 -0700

    try to understand error on GA

--- R/Require-helpers.R
+++ R/Require-helpers.R
@@ -2878,7 +2878,9 @@ extractPkgNameFromFileName <- function(x) {
 appendToWarns <- function(w, warns, Package) {
   pkgName <- unique(unlist(lapply(Package, function(p) p[grepl(p, w)])))
   # pkgName <- extractPkgNameFromFileName(w)
+  pkgName <- na.omit(pkgName)
   newWarn <- list(w)
+  if (length(pkgName) != 1) stop(paste(pkgName, collapse = ", "))
   names(newWarn) <- pkgName
   append(warns, newWarn)
 }
