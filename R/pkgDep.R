7c42f0941f53d426a4eba434ea796579c53c787a
Eliot McIntire <eliotmcintire@gmail.com>
2022-10-06 22:27:55 -0700

    Revert "pkgDep -- bugfix when .install.pkgs is empty"
    This reverts commit 25169a77e5b636ff8b39d1a4b0cdd133e2482663.

--- R/pkgDep.R
+++ R/pkgDep.R
@@ -672,6 +672,7 @@ whichToDILES <- function(which) {
     if (any(grepl("github", tolower(other)))) {
       other <- c("GithubRepo", "GithubUsername", "GithubRef", "GithubSHA1")
     }
+
   out <- lapply(lib.loc, function(path) {
     dirs <- dir(path, full.names = TRUE)
     areDirs <- dir.exists(dirs)
@@ -699,13 +700,11 @@ whichToDILES <- function(which) {
   out <- do.call(rbind, out)
   ret <- cbind("Package" = basename(unlist(out[, "Package"])), "LibPath" = rep(lib.loc, lengths),
                "Version" = out[, "Version"])
-  if (NROW(ret)) {
-    if (length(which))
-      ret <- cbind(ret, "Dependencies" = out[, "Depends"], stringsAsFactors = FALSE)
-    if (!is.null(other)) {
-      ncolBefore <- NCOL(ret)
-      ret <- cbind(ret, out[, other], stringsAsFactors = FALSE)
-    }
+  if (length(which))
+    ret <- cbind(ret, "Dependencies" = out[, "Depends"], stringsAsFactors = FALSE)
+  if (!is.null(other)) {
+    ncolBefore <- NCOL(ret)
+    ret <- cbind(ret, out[, other], stringsAsFactors = FALSE)
   }
   ret
 }
