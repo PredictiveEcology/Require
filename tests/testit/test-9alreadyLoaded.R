61ec3ce76424b3f83a66c0834c358305e9a19f31
Eliot McIntire <eliotmcintire@gmail.com>
2022-10-15 13:47:44 -0700

    debugging test 9

--- tests/testit/test-9alreadyLoaded.R
+++ tests/testit/test-9alreadyLoaded.R
@@ -9,22 +9,25 @@ if (isDev && Require:::isWindows()) {
 
 # Trying to install a package whose dependency is a loadedNamespace, but that is not available
   ####
-  out6 <- capture.output(type = "message",
-                         out <- suppressWarnings(Require("whisker",
+  # out6 <- capture.output(type = "message",
+  #                        out <- suppressWarnings(
+                           Require("whisker",
                                                          require = TRUE,
-                                                         standAlone = TRUE))) # warnings would be if whisker is used by e.g., devtools
-  out3 <- capture.output(type = "message",
-                         out5 <- capture.output(out4 <- suppressWarnings(
-                           remove.packages("whisker")), silent = TRUE))
+                                                         standAlone = TRUE)
+                           # )) # warnings would be if whisker is used by e.g., devtools
+  # out3 <- capture.output(type = "message",
+                         # out5 <- capture.output(out4 <- suppressWarnings(
+                           remove.packages("whisker")
+                           #), silent = TRUE))
   warns <- list()
-  mess <- capture.output(type = "message",
-                         out2 <- withCallingHandlers(
+  # mess <- capture.output(type = "message",
+  #                        out2 <- withCallingHandlers(
                            Require("PredictiveEcology/SpaDES.project@development", require = FALSE, verbose = 2)
-                           , warning = function(w) {
-                             warns <<- Require:::appendToWarns(w$message, warns, Package = c("whisker", "SpaDES.project"))
-                             invokeRestart("muffleWarning")
-                           })
-  )
+  #                          , warning = function(w) {
+  #                            warns <<- Require:::appendToWarns(w$message, warns, Package = c("whisker", "SpaDES.project"))
+  #                            invokeRestart("muffleWarning")
+  #                          })
+  # )
   testit::assert(any(grepl("is in use", warns)))
   testit::assert(any(grepl("is in use", mess)))
 
