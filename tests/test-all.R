5d9dbeb35c8483e2695826ef887ee8eac077abde
Eliot McIntire <eliotmcintire@gmail.com>
2022-10-14 23:55:01 -0700

    test-all -- take out of runTests -- too hard to debug on GA

--- tests/test-all.R
+++ tests/test-all.R
@@ -38,32 +38,33 @@ source(dir(pattern = "test-helpers.R", recursive = TRUE, full.names = TRUE))
 # }
 
 # Put this in a function so we can wrap it in a try, so that we can revert everything
-runTests <- function(checks) {
-  ## force tests to be executed if in dev release which we define as
-  ## having a sub-release, eg 0.9.15.5 is one whereas 0.9.16 is not
-  ee <- new.env()
+#runTests <- function(checks) {
+## force tests to be executed if in dev release which we define as
+## having a sub-release, eg 0.9.15.5 is one whereas 0.9.16 is not
+ee <- new.env()
 
-  startTimeAll <- startTime <- Sys.time()
-  tdOuter <- Require::tempdir2("tests")
-  try(saveRDS(startTimeAll, file = file.path(tdOuter, "startTimeAll")), silent = TRUE)
+startTimeAll <- startTime <- Sys.time()
+tdOuter <- Require::tempdir2("tests")
+try(saveRDS(startTimeAll, file = file.path(tdOuter, "startTimeAll")), silent = TRUE)
 
-  optsListNew <- list()
-  verbosity <- if (isDevAndInteractive) 1 else -2
-    # options(Require.RPackageCache = FALSE)
+optsListNew <- list()
+verbosity <- if (isDevAndInteractive) 1 else -2
+# options(Require.RPackageCache = FALSE)
 
-  if (!startsWith(getOption("repos")[[1]], "http")) # deal with @CRAN@
-    origRepos <- options(repos = c(CRAN = Require:::srcPackageURLOnCRAN))
+if (!startsWith(getOption("repos")[[1]], "http")) # deal with @CRAN@
+  origRepos <- options(repos = c(CRAN = Require:::srcPackageURLOnCRAN))
 
-  optsListPrevLinux <- if (isDevAndInteractive) Require::setLinuxBinaryRepo() else NULL
+optsListPrevLinux <- if (isDevAndInteractive) Require::setLinuxBinaryRepo() else NULL
 
-  optsListNew <- Require::modifyList2(optsListNew, list(Require.verbose = verbosity),
-                                      keep.null = TRUE)
-  optsNcpus <- options("Ncpus" = 4)
-  optsListPrev <- options(optsListNew)
-  # optsListNew <- modifyList2(optsListNew, options("repos"))
-  origLibPaths <- .libPaths()
-  origWd <- getwd()
+optsListNew <- Require::modifyList2(optsListNew, list(Require.verbose = verbosity),
+                                    keep.null = TRUE)
+optsNcpus <- options("Ncpus" = 4)
+optsListPrev <- options(optsListNew)
+# optsListNew <- modifyList2(optsListNew, options("repos"))
+origLibPaths <- .libPaths()
+origWd <- getwd()
 
+if (FALSE) { # Don't run this; too hard to debug when runTests is a function
   on.exit({
     currOptions <- options()
     toRevert <- Require::setdiffNamed(currOptions, origOptions, missingFill = NULL)
@@ -165,11 +166,109 @@ runTests <- function(checks) {
     # testit::assert()
 
   }, add = TRUE)
+}
+# The test
+test_pkg("Require")
 
-  # The test
-  test_pkg("Require")
+#}
 
+#runTests(checks)
+
+currOptions <- options()
+toRevert <- Require::setdiffNamed(currOptions, origOptions, missingFill = NULL)
+toRevert <- toRevert[grep("^datatable", names(toRevert), value = TRUE, invert = TRUE)] # don't revert data.table options
+
+envCur <- Sys.getenv()
+envNeedRm <- envCur[!names(envCur) %in% names(envOrig)]
+
+checks$prior <- list()
+checks$prior[["getwd"]] <- getwd()
+checks$prior[["options"]] <- options()
+checks$prior[["libPaths"]] <- .libPaths()
+checks$prior[["cacheDir"]] <- dir(Require::RequireCacheDir(), recursive = TRUE)
+checks$prior[["envVars"]] <- Sys.getenv()
+checks$prior[["tempdir2"]] <- dir(Require::tempdir2(), recursive = TRUE)
+
+endTime <- Sys.time()
+try(Require:::messageVerbose("\033[32m ----------------------------------All Tests: ",format(endTime - startTimeAll)," \033[39m",
+                             verbose = getOption("Require.verbose"), verboseLevel = -1),
+    silent = TRUE)
+
+fis <- ls(ee)
+names(fis) <- basename(fis)
+times <- lapply(fis, function(fi) ee[[fi]])
+times <- data.table::data.table(times)
+data.table::set(times, NULL, "file", names(fis))
+Require:::messageDF(times, verboseLevel = 0, verbose = getOption("Require.verbose"))
+
+
+# six things to clean up: libPaths, wd, options, cached pkgs, 5: envir vars; tempdir2
+# 1 libPaths
+.libPaths(origLibPaths)
+
+# 2 wd
+setwd(origWd)
+
+# 3 options
+options(toRevert)
+
+# 4 Cache -- remove everything that was added in this test
+if (!isDev) {
+  if (length(checks$start[["cacheDir"]]) == 0)
+    try(unlink(checks$prior[["cacheDir"]], recursive = TRUE))
+  else
+    try(unlink(setdiff(checks$start[["cacheDir"]], checks$prior[["cacheDir"]])))
 }
 
-runTests(checks)
+# 5 Sys.env
+envVarsChanged <- c("CRANCACHE_DISABLE", "R_REMOTES_UPGRADE", "Require.RPackageCache",
+                    "RequireRunAllTests", "R_TESTS")
+envVarsChangedNeedUnset <- names(envCur)[names(envCur) %in% envVarsChanged]
+if (length(envVarsChangedNeedUnset))
+  Sys.unsetenv(envVarsChangedNeedUnset)
 
+if (FALSE) { # This will try to unset all env vars; this is too broad as there seem to be many that are set by GA
+  ma <- match(names(envOrig), names(envCur) )
+  envNeedRevert <- envCur[ma] != envOrig
+  if (any(na.omit(envNeedRevert))) {
+    envNeedRevert <- envOrig[envNeedRevert]
+  }
+  Sys.unsetenv(names(envNeedRm))
+}
+
+# 6 tempdir2
+unlink(Require::tempdir2(), recursive = TRUE)
+
+checks$post <- list()
+checks$post[["getwd"]] <- getwd()
+checks$post[["options"]] <- options()
+checks$post[["libPaths"]] <- .libPaths()
+checks$post[["cacheDir"]] <- dir(Require::RequireCacheDir(FALSE), recursive = TRUE)
+checks$post[["envVars"]] <- Sys.getenv()
+checks$post[["tempdir2"]] <- dir(Require::tempdir2(), recursive = TRUE)
+
+Require:::messageVerbose("Done tests", verboseLevel = -2, verbose = verbosity)
+
+# Check everything is reset to original
+if (FALSE) {
+  if (isDev) {
+    nam <- names(checks$start)
+    hashes <- "########################################################################"
+    compare <- lapply(nam, function(x) {
+      a <- checks$start[[x]]
+      if (!is.null(names(a))) a <- a[order(names(a))]
+      b <- checks$post[[x]]
+      if (!is.null(names(b))) b <- b[order(names(b))]
+      ae <- all.equal(a, b)
+      if (!isTRUE(ae)) {
+        print(paste(x, hashes))
+        try(print(setdiffNamed(a, b, missingFill = NULL)), silent = TRUE)
+        try(print(setdiffNamed(a, b, missingFill = "")), silent = TRUE)
+      }
+      ae
+    })
+    # theTest <- all(unlist(compare))
+  }
+}
+# if (!isTRUE(theTest)) browser()
+# testit::assert()
