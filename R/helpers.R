f04a3cca6215a2edc69e28e6e5281d62b63757b6
Eliot McIntire <eliotmcintire@gmail.com>
2022-11-12 20:52:05 -0800

    penultimate changes

--- R/helpers.R
+++ R/helpers.R
@@ -472,7 +472,7 @@ messageVerboseCounter <-
 #' fail to install a package with an error of source file not found.
 #' See: <https://github.com/HenrikBengtsson/startup/issues/19>.
 #'
-#' The env variable is set here:
+#' The environment variable is set here:
 #' `https://github.com/wch/r-source/blob/8b6429feb661b02e2b2b6df1757b31cf1250a33e/src/library/tools/R/testing.R#L472-Lundefined`
 #'
 #' @keywords internal
