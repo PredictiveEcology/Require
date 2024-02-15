9e6085f8cba33fd024dcf6fa35a735b16ca4f220
Eliot McIntire <eliotmci@mail.ubc.ca>
2024-02-15 12:15:52 -0800

    start using testthat

---
+++ tests/testthat/setup.R
@@ -0,0 +1,14 @@
+
+isDev <- Sys.getenv("R_REQUIRE_RUN_ALL_TESTS") == "true" &&
+  Sys.getenv("R_REQUIRE_CHECK_AS_CRAN") != "true"
+# Actually interactive
+isDevAndInteractive <- interactive() && isDev && Sys.getenv("R_REQUIRE_TEST_AS_INTERACTIVE") != "false"
+
+if (!isDevAndInteractive) { # i.e., CRAN
+  Sys.setenv(R_REQUIRE_PKG_CACHE = "FALSE")
+}
+
+withr::local_options(
+  list(Require.isDev = isDev, isDevAndInteractive = isDevAndInteractive),
+  .local_envir = teardown_env()
+)
