775b57ad62f5a7fb9ddf2276d63134417fb16c29
Eliot McIntire <eliotmcintire@gmail.com>
2024-04-10 17:48:57 -0700

    test-8

--- tests/testthat/test-08modules_testthat.R
+++ tests/testthat/test-08modules_testthat.R
@@ -154,9 +154,10 @@ test_that("test 5", {
       )
     }
 
-    ip <- installed.packages(noCache = TRUE) |> as.data.table()
+    ip <- installed.packages(dirForInstall, noCache = TRUE) |> as.data.table()
 
-    allInstalled <- setdiff(setdiff(trimRedundancies(pkgs)$Package, c("Require", "data.table")),
+    allInstalled <- setdiff(setdiff(trimRedundancies(pkgs)$Package,
+                                    extractPkgName(c(.RequireDependencies, .basePkgs))),
                             ip$Package)
     a <- attr(out[[i]], "Require")
     expect_true(length(allInstalled) == 0)
