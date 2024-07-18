9b3dae92093d075a15aa443eb64aa193b3a64f68
Eliot McIntire <eliotmcintire@gmail.com>
2024-07-18 16:13:03 -0700

    setup

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,8 +1,8 @@
 if (.isDevelVersion() && nchar(Sys.getenv("R_REQUIRE_RUN_ALL_TESTS")) == 0) {
   Sys.setenv("R_REQUIRE_RUN_ALL_TESTS" = "true")
 }
-verboseForDev <- 1
-Require.usePak <- TRUE
+verboseForDev <- 2
+Require.usePak <- FALSE
 
 isDev <- Sys.getenv("R_REQUIRE_RUN_ALL_TESTS") == "true" &&
   Sys.getenv("R_REQUIRE_CHECK_AS_CRAN") != "true"
