13545fad93685926e3494b8dc68af44d888c0b81
Eliot McIntire <eliotmci@mail.ubc.ca>
2024-08-01 14:26:48 -0700

    typo

--- R/Require2.R
+++ R/Require2.R
@@ -1872,8 +1872,8 @@ getGitHubVersionOnRepos <- function(pkgGitHub) {
     pkgGitHub <- dlGitHubFile(pkgGitHub)
     dFile <- pkgGitHub[["DESCFile"]]
     hasDFile <- which(!is.na(dFile))
-    set(pkgGitHub, hasDFile, "VersionOnRepos",  DESCRIPTIONFileVersionV(dFile[hasDfile]))
-    mayNeedPackageNameChange <- DESCRIPTIONFileOtherV(dFile[hasDfile], other = "Package")
+    set(pkgGitHub, hasDFile, "VersionOnRepos",  DESCRIPTIONFileVersionV(dFile[hasDFile]))
+    mayNeedPackageNameChange <- DESCRIPTIONFileOtherV(dFile[hasDFile], other = "Package")
     alreadyCorrect <- pkgGitHub[["Package"]][hasDFile] == mayNeedPackageNameChange
     notAlreadyCorrect <- alreadyCorrect %in% FALSE
     if (any(notAlreadyCorrect)) {
