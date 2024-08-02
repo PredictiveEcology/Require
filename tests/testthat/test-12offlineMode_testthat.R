d2eeec150e894e446d908baa1abace106844f1ba
Eliot McIntire <eliotmci@mail.ubc.ca>
2024-08-01 17:16:14 -0700

    add test-12 offlineMode

---
+++ tests/testthat/test-12offlineMode_testthat.R
@@ -0,0 +1,39 @@
+test_that("test12 Require.offlineMode", {
+
+  # skip_if(getOption("Require.usePak"), message = "Not an option on usePak = TRUE")
+  setupInitial <- setupTest()
+
+  isDev <- getOption("Require.isDev")
+  isDevAndInteractive <- getOption("Require.isDevAndInteractive")
+  fpcs <- c("fpCompare", "PredictiveEcology/fpCompare")
+  cachePurge()
+  for (fpc in fpcs) {
+    fpcPkgName <- extractPkgName(fpc)
+    cacheClearPackages(fpcPkgName, ask = FALSE)
+    Install(fpc)
+    mess <- capture_messages(remove.packages(fpcPkgName))
+    options(Require.offlineMode = TRUE)
+    Install(fpc)
+    expect_true(base::require(fpcPkgName, quietly = TRUE, character.only = TRUE))
+    detach(name = paste0("package:", fpcPkgName), unload = TRUE, character.only = TRUE)
+    # expect_match(basename(find.package(fpcPkgName)), fpcPkgName)
+    mess <- capture_messages(remove.packages(fpcPkgName))
+    expect_false(base::require(fpcPkgName, quietly = TRUE, character.only = TRUE))
+    cacheClearPackages(fpcPkgName, ask = FALSE)
+    warns <- capture_warnings(Install(fpc))
+    expect_false(base::require(fpcPkgName, quietly = TRUE, character.only = TRUE))
+    expect_match(warns, .txtCouldNotBeInstalled)
+    options(Require.offlineMode = FALSE)
+    # cacheClearPackages(extractPkgName(fpc), ask = FALSE)
+    Install(fpc)
+    expect_true(base::require(fpcPkgName, quietly = TRUE, character.only = TRUE))
+    detach(name = paste0("package:", fpcPkgName), unload = TRUE, character.only = TRUE)
+    mess <- capture_messages(remove.packages(fpcPkgName))
+    Install(fpc)
+    expect_true(base::require(fpcPkgName, quietly = TRUE, character.only = TRUE))
+    detach(name = paste0("package:", fpcPkgName), unload = TRUE, character.only = TRUE)
+    mess <- capture_messages(remove.packages(fpcPkgName))
+
+  }
+
+})
