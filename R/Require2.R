1db303f4c7de613d5a56bf934f8797126bbef3ec
Eliot McIntire <eliotmci@mail.ubc.ca>
2025-03-01 12:14:42 -0800

    missing repos = repos

--- R/Require2.R
+++ R/Require2.R
@@ -1726,7 +1726,8 @@ availablePackagesOverride <- function(toInstall, repos, purge, type = getOption(
       ap3[, "Repository"] <- toInstall[Package %in% pkgsNotInAP]$Repository
     }
     ap3[, "Depends"] <- NA
-    deps <- pkgDep(toInstall[Package %in% pkgsNotInAP][["packageFullName"]], recursive = TRUE,
+    deps <- pkgDep(toInstall[Package %in% pkgsNotInAP][["packageFullName"]],
+                   recursive = TRUE, repos = repos,
                    verbose = verbose)
     pkgHasNameDiffrntThanRepo <- extractPkgName(names(deps)) != toInstall[Package %in% pkgsNotInAP][["Package"]]
     if (any(pkgHasNameDiffrntThanRepo)) {
